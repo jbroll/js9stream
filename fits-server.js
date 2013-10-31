01845877ba1f4ed7c10ac6f25214660ba30507c5
john@cfa.harvard.edu <john@cfa.harvard.edu>
2013-10-31 14:54:13 -0400

    initial commit

---
+++ fits-server.js
@@ -0,0 +1,60 @@
+
+
+var server = require('http').Server();
+var websok = require('websocket').server;
+var fs     = require('fs');
+
+
+ws = new websok({
+      httpServer: server
+    , fragmentOutgoingMessages: false
+})
+
+ws.on('request', function(request) {
+
+    if ( request.requestedProtocols[0] != 'fits-stream' ) {
+	request.reject(404, "Wrong Protocol")
+    }
+
+    var sock = request.accept('fits-stream', request.origin)
+
+    console.log('Conneciton Accepted')
+
+    var count = 0
+
+    sock.on('message', function(message) {
+	//console.log('Message : ', message)
+
+	count %= 720;
+	count += 1;
+
+	if ( message.type == "utf8" && message.utf8Data == "getimage" ) {
+	    var image = "images/im." + count + ".fits"
+
+	    //console.log(image)
+
+	    fs.open(image, 'r',
+		function(status, fd) {
+		    if ( status ) { 
+			console.log("status", status)
+			return;
+		    }
+
+		    stat = fs.fstatSync(fd)
+
+		    buffer = new Buffer(stat.size)
+
+		    fs.read(fd, buffer, 0, stat.size, 0, function() {
+			fs.close(fd)
+			sock.send(buffer)
+		    })
+
+		}
+	    )
+	}
+    })
+})
+
+
+server.listen(3000);
+
